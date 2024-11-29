5603e6067ded77b5cd92e7f21921bb70caf62135
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2024-11-28 21:09:16 -0300

    Prova Programação Web

---
+++ ThallesNascimentoReceptorMS/src/main/java/com/senac/ThallesNascimentoReceptorMS/models/VooModel.java
@@ -0,0 +1,11 @@
+package com.senac.ThallesNascimentoEmissorMS.models;
+
+public class VooModel {
+    private static final long serialVersionUID = 1L;
+
+    private Long voosId;
+    private String voo;
+
+
+
+}
