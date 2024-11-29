b25754def0fc8105cba7d4345699fb767a4999a7
ADS 2023.1N <36129382023.1N@senacrjedu.local>
2024-11-28 21:12:36 -0300

    Prova Programação Web

--- ThallesNascimentoEmissorMS/src/main/java/com/senac/ThallesNascimentoEmissorMS/models/VooModel.java
+++ ThallesNascimentoEmissorMS/src/main/java/com/senac/ThallesNascimentoEmissorMS/models/VooModel.java
@@ -6,6 +6,19 @@ public class VooModel {
     private Long voosId;
     private String voo;
 
+    public Long getVoosId() {
+        return voosId;
+    }
 
+    public void setVoosId(Long voosId) {
+        this.voosId = voosId;
+    }
 
+    public String getVoo() {
+        return voo;
+    }
+
+    public void setVoo(String voo) {
+        this.voo = voo;
+    }
 }
